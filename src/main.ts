270300b63762c9dc55c38af23b44c6d996d19289
Andrea Betancourt <andreabetancourt93@hotmail.com>
2024-01-10 08:38:28 -0500

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   // Configura para que la informacion sea enviada de acuerdo a las especificaciones del back
   app.useGlobalPipes( new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, }) );
 
