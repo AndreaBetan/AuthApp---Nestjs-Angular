dbfd043cdd931abebc2c9adfa86fd10bf94142b9
Andrea Betancourt <andreabetancourt93@hotmail.com>
2024-01-10 16:03:37 -0500

    Update environment variables

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   // Configura para que la informacion sea enviada de acuerdo a las especificaciones del back
   app.useGlobalPipes( new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, }) );
 
-  await app.listen(3000);
+  // Si la app tiene una variable de entorno con el puerto, lo ejecuta ahi de lo contrario usa el 3000
+  await app.listen( process.env.PORT ?? 3000);
 }
 bootstrap();
